46140ada1caa7714267936b521e246c22ccca007
anas benlemlih <anas.benlemlih@gmail.com>
2025-09-13 21:49:09 +0200

    backend spring with dependencies

---
+++ backend/src/main/java/com/aben/locme/LocmeApplication.java
@@ -0,0 +1,13 @@
+package com.aben.locme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocmeApplication.class, args);
+	}
+
+}
